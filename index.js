953e1f5db7155f0b73b36dc073fa414b93cae1e1
Neron Kevin <neronkevin@gmail.com>
2022-12-02 12:38:48 +0100

    Challenge version 1

---
+++ index.js
@@ -0,0 +1,8 @@
+//Calculer le coût total des cadeaux pour noël de la famille Smith pour l'année 2022, 2021 et 2020, puis faire la moyenne des totaux.
+
+const year2020 = [100, 97, 3, 25, 89, 349, 18, 62, 44, 48, 30];
+const year2021 = [3, 99, 29, 56, 161, 93, 46, 22, 19, 11];
+const year2022 = [999, 292, 100, 1, 3, 88, 66, 28, 99, 17, 64, 81];
+
+// Résultat attendu
+//Le cout de l'année 2022 est de 1838, et la moyenne des coûts est de 1081 (chiffre arrondis au supérieur)
