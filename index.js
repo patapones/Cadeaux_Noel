fed1fb2cf61a714aa4b261f5e3fcff0b850d6bea
Neron Kevin <neronkevin@gmail.com>
2022-12-02 13:53:04 +0100

    Challenge V2, calcul total des cadeaux par an et de la moyenne

--- index.js
+++ index.js
@@ -6,3 +6,34 @@ const year2022 = [999, 292, 100, 1, 3, 88, 66, 28, 99, 17, 64, 81];
 
 // Résultat attendu
 //Le cout de l'année 2022 est de 1838, et la moyenne des coûts est de 1081 (chiffre arrondis au supérieur)
+
+// Faire la somme pour chaque année
+function sumOneYear(years) {
+  const initialValue = 0;
+  const year = years.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue
+  );
+  return year;
+}
+
+const resultSumEachYear = console.log(
+  `Le coût de l'année 2020 est de ${sumOneYear(
+    year2020
+  )}, celui de 2021 de ${sumOneYear(
+    year2021
+  )}, et celui de 2022 de ${sumOneYear(year2022)}`
+);
+
+resultSumEachYear;
+
+const averageSum =
+  sumOneYear(year2020) / (year2020.length * 1) +
+  sumOneYear(year2021) / (year2021.length * 1) +
+  sumOneYear(year2022) / (year2022.length * 1);
+
+const totalAverage = console.log(
+  `La moyenne des coût est de ${Math.round(averageSum)}`
+);
+
+totalAverage;
